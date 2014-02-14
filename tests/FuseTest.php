4790ebdf300c28a33ea3c3ab94f5fcd69cdfb90a
Sairiz <saiffil@mtouch.my>
2014-02-14 13:29:53 +0800

    Making sure everything works perfect

--- tests/FuseTest.php
+++ tests/FuseTest.php
@@ -5,7 +5,7 @@ class FuseTest extends TestCase {
 	public function testInfusionsoftConnection()
 	{
 		$data = Fuse::appEcho('test');
-		
+
 	  	$this->assertEquals('ERROR: -1 - No method matching arguments: java.lang.String, java.lang.String', $data);
 	}
 }
